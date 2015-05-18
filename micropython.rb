6251f620289de3dd8680b96da0dcc40773ea0f22
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2015-05-18 22:45:22 +0200

    Add micropython formula

---
+++ micropython.rb
@@ -0,0 +1,39 @@
+class Micropython < Formula
+  homepage "https://github.com/micropython/micropython"
+  head "https://github.com/micropython/micropython.git"
+
+  resource "micropython-lib" do
+    url "https://github.com/micropython/micropython-lib.git"
+  end
+
+  depends_on "pkg-config" => :build
+  # micropython-lib requires GNU core utils
+  # https://github.com/micropython/micropython-lib/issues/10
+  depends_on "findutils" => [:build]
+  depends_on "coreutils" => [:build]
+
+  depends_on "libffi"
+  depends_on "readline"
+
+  def install
+    cd "unix" do
+      system "make", "PREFIX=#{prefix}"
+      libexec.install "micropython"
+    end
+
+    (bin/"micropython").write_env_script(libexec/"micropython", :MICROPYPATH => "~/.micropython/lib:#{lib}/micropython")
+
+    resource("micropython-lib").stage do
+      inreplace "Makefile" do |s|
+        s.gsub! "find", "gfind"
+        s.gsub! "cp", "gcp"
+        s.gsub! "xargs", "gxargs"
+      end
+      system "make", "install", "PREFIX=#{lib}/micropython"
+    end
+  end
+
+  test do
+    system "#{bin}/micropython", "-c", "import os"
+  end
+end
